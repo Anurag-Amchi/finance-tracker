a1f27862775efdbf52aaa4abc4f6afeeabc79dcc
Anurag <anuragamchi@gmail.com>
2025-06-23 01:14:40 +0530

    deployment changes

--- backend/server.mjs
+++ backend/server.mjs
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser';
 import connectDB from './config/db.mjs';
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 dotenv.config();
